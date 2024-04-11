2bd87f7792732338d65d55e3f89af8f269c3950e
leomar <leomar.rodriguez1@upr.edu>
2024-04-11 19:24:38 -0400

    deleted space after HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,7 +11,7 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB '
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
