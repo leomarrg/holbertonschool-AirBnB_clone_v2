018e4de58bcc2172f80028f1120418ced9be3242
leomar <leomar.rodriguez1@upr.edu>
2024-04-11 22:40:42 -0400

    fixed pycodestyle errors

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c_text(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python_text(text = "is cool"):
+def python_text(text="is cool"):
     text = text.replace("_", " ")
     return "Python {}".format(text)
 
